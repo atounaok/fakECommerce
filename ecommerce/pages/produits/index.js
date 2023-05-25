d936c64ee4858b5692a8ce8e0473405d10a759f8
atounaok <atounaok@gmail.com>
2023-05-24 20:51:50 -0400

    :hammer: Avancements

--- ecommerce/pages/produits/index.js
+++ ecommerce/pages/produits/index.js
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
 
+import { MdOutlineFavoriteBorder, MdOutlineFavorite } from 'react-icons/md'
+
 
 export const getStaticProps = async () => {
     const res = await fetch('https://fakestoreapi.com/products')
@@ -25,11 +27,17 @@ const index = ({ produits }) => {
                 <div className='flex flex-wrap justify-between items-center'>
                     {
                         produits.map(produit => (
-                            <Link className='card w-[390px] h-[400px] mb-20 flex flex-col justify-start items-center hover:shadow-sm' key={produit.id} href={'/produits/' + produit.id}>
+                            <Link className='card w-[290px] min-h-[400px] mb-20 flex flex-col justify-start items-center hover:shadow-sm' key={produit.id} href={'/produits/' + produit.id}>
                                 <div className='h-[200px] w-full flex justify-center items-center'>
                                     <Image loader={() => produit.image} src={produit.image} alt={'produit: ' + produit.id} width='100' height='100'/>
                                 </div>
-                                <h3 className='text-3xl text-start text-wrap line-clamp-2'>{produit.title}</h3>
+                                <div className='flex flex-col justify-between h-[180px]'>
+                                    <h3 className='w-full text-2xl text-start text-wrap line-clamp-2'>{produit.title}</h3>
+                                    <div className='flex justify-between w-full'>
+                                        <p className='text-left text-2xl font-thin'>{produit.price}$</p>
+                                        <MdOutlineFavoriteBorder className='text-2xl'/>
+                                    </div>
+                                </div>
                             </Link>
                         ))
                     }
